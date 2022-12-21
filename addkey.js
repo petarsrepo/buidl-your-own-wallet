70070fc4eccebfedbf5a60f641b97d660d917f2a
PeteStoik <petar.i.stoykov@gmail.com>
2022-12-21 18:12:21 +0200

    Apply validation fixes

--- addkey.js
+++ addkey.js
@@ -4,16 +4,17 @@ const fs = require("fs")
 const web3 = new Web3(Web3.givenProvider || process.env.ENDPOINT);
 
 // prepare output
-const newkey = fs.readFileSync("./dls/newkey.json")
-
-// check input validity
-if (web3.utils.isHexStrict(newkey)) {
+const newkey = fs.readFileSync("./dls/newkey.json", 'utf8')
+var valid = web3.utils.isHexStrict(newkey);
+console.log(valid);
+if (valid === true) {
+  console.log('test');
 const wallet = web3.eth.accounts.wallet.add(newkey)
 console.log(wallet.address);
 
 // ammend .env for other script access
 const endpointENV = "ENDPOINT=" + '"' + process.env.ENDPOINT + '"'
-const pkeyENV = "PRIVATE_KEY=" + '"' + fs.readFileSync("./dls/newkey.json") + '"'
+const pkeyENV = "PRIVATE_KEY=" + '"' + fs.readFileSync("./dls/newkey.json", 'utf8') + '"'
 const pkeypassENV = "PRIVATE_KEY_PASS=" + '"' + process.env.PRIVATE_KEY_PASS + '"'
 const changesENV = endpointENV + "\n" + pkeyENV + "\n" + pkeypassENV
 fs.writeFileSync("./.env", changesENV)
@@ -22,7 +23,7 @@ fs.writeFileSync("./.env", changesENV)
 fs.writeFileSync("./dls/addkey.json", JSON.stringify(wallet));
 fs.writeFileSync("./pages/addkey.html", JSON.stringify(wallet) + '<form action="/dls/addkey.json" method="GET">' + '<button id="addkeyDL" onclick="loadAddkeyDL()">Download</button>' + '</form>');
 } else {
-  
+
 // pass error if input ivalid
 fs.writeFileSync("./pages/addkey.html", "<strong>Input error: Invalid private key</strong>" + "</br>Confirm you have entered your private key correctly." + "</br>Don't forget to add '0x' as prefix to your private key to pass the validation!" + "</br>Example: 0xe515a43eed6a3f06221dff872f00eada462122949e2018abce8d0c8a539122c7</br>" + `<button id="tryAgain" onclick="window.location='../'">Try again</button>`);
 }
