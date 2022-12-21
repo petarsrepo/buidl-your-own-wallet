5d8cb2974c8c525b905a9bbbdc706c7f4210c661
PeteStoik <petar.i.stoykov@gmail.com>
2022-12-21 15:42:57 +0200

    1

--- balance.js
+++ balance.js
@@ -13,10 +13,10 @@ const balanceGwei = web3.utils.fromWei(balance, 'gwei');
 const balanceEths = web3.utils.fromWei(balance, 'ether');
 
 // write as page for web viewing
-fs.writeFileSync("./pages/balance.html", "The balance of your " + address + "wallet is: " + balance + ' wei\n Balance in gwei: ' + balanceGwei + ' gwei\n Balance in Ethers: ' + balanceEths + ' ETH' + '<form action="/dls/balance.json" method="GET">' + '<button id="balanceDL" onclick="loadBalanceDL()">Download</button>' + '</form>');
+fs.writeFileSync("./pages/balance.html", "The balance of your " + address + " wallet is: " + balance + ' wei\n Balance in gwei: ' + balanceGwei + ' gwei\n Balance in Ethers: ' + balanceEths + ' ETH' + '<form action="/dls/balance.json" method="GET">' + '<button id="balanceDL" onclick="loadBalanceDL()">Download</button>' + '</form>');
 
 // write as JSON for download
-fs.writeFileSync("./dls/balance.json", "The balance of your " + address + "wallet is: " + balance + ' wei\n Balance in gwei: ' + balanceGwei + ' gwei\n Balance in Ethers: ' + balanceEths + ' ETH');
+fs.writeFileSync("./dls/balance.json", "The balance of your " + address + " wallet is: " + balance + ' wei\n Balance in gwei: ' + balanceGwei + ' gwei\n Balance in Ethers: ' + balanceEths + ' ETH');
 };
 getbal(wallet.address)
 .then(() => process.exit(0))
